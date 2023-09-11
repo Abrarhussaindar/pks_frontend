d1a301c19c796bbf9d580ad7d9e820e1f989276c
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2023-09-11 10:14:31 +0530

    secondary navbar

---
+++ src/components/secondaryNavbar/SecondaryNavabar.jsx
@@ -0,0 +1,19 @@
+// import { Link } from "react-router-dom"
+import "./secondaryNavbar.css"
+
+export default function SecondaryNavabar() {
+    return (
+        <div className="secondaryNavbarContainer">
+            <img src="/assets/images/cate_imgs/man.png" alt="" />
+            <img src="/assets/images/cate_imgs/woman.png" alt="" />
+            <img src="/assets/images/cate_imgs/kids.png" alt="" />
+            <img src="/assets/images/cate_imgs/phone.png" alt="" />
+            <img src="/assets/images/cate_imgs/electronics.png" alt="" />
+            <img src="/assets/images/cate_imgs/sports.png" alt="" />
+            <img src="/assets/images/cate_imgs/toys.png" alt="" />
+            <img src="/assets/images/cate_imgs/grocery.png" alt="" />
+            <img src="/assets/images/cate_imgs/beauty.png" alt="" />
+            {/* <img src="/assets/images/cate_imgs/man.png" alt="" /> */}
+        </div>
+    )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,13 @@ import Footer from "../../components/footer/Footer";
 import HomeSuggetsProducts from "../../components/homeSuggestProducts/HomeSuggetsProducts";
 import Navbar from "../../components/navbar/Navbar";
 import RecentProductsContainer from "../../components/recentProductsContainer/RecentProductsContainer";
+import SecondaryNavabar from "../../components/secondaryNavbar/SecondaryNavabar";
 
 export default function Home() {
     return (
         <>
             <Navbar />
+            <SecondaryNavabar />
             <div className="mainContent">
                 <HomeBanner />
                 <RecentProductsContainer />

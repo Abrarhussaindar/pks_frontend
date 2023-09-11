c46577f74bf2b2d927afa968565b6ad540d72c17
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2023-09-11 12:27:41 +0530

    few changes in sec navbar

--- src/components/secondaryNavbar/SecondaryNavabar.jsx
+++ src/components/secondaryNavbar/SecondaryNavabar.jsx
@@ -4,33 +4,70 @@ import "./secondaryNavbar.css";
 export default function SecondaryNavabar() {
     return (
         <div className="secondaryNavbarContainer">
-            <Link to=".">
-                <img src="/assets/images/cate_imgs/man.png" alt="" />
-            </Link>
-            <Link to=".">
-                <img src="/assets/images/cate_imgs/woman.png" alt="" />
-            </Link>
-            <Link to=".">
-                <img src="/assets/images/cate_imgs/kids.png" alt="" />
-            </Link>
-            <Link to=".">
-                <img src="/assets/images/cate_imgs/phone.png" alt="" />
-            </Link>
-            <Link to=".">
-                <img src="/assets/images/cate_imgs/electronics.png" alt="" />
-            </Link>
-            <Link to=".">
-                <img src="/assets/images/cate_imgs/sports.png" alt="" />
-            </Link>
-            <Link to=".">
-                <img src="/assets/images/cate_imgs/toys.png" alt="" />
-            </Link>
-            <Link to=".">
-                <img src="/assets/images/cate_imgs/grocery.png" alt="" />
-            </Link>
-            <Link to=".">
-                <img src="/assets/images/cate_imgs/beauty.png" alt="" />
-            </Link>
+            <div className="container">
+
+                <Link to=".">
+                    <img src="/assets/images/cate_imgs/man.png" alt="" />
+                    <span>Men's</span>
+                </Link>
+            </div>
+            <div className="container">
+                <Link to=".">
+                    <img src="/assets/images/cate_imgs/woman.png" alt="" />
+                    <span>Women's</span>
+                </Link>
+
+            </div>
+            <div className="container">
+                <Link to=".">
+                    <img src="/assets/images/cate_imgs/kids.png" alt="" />
+                    <span>Kids</span>
+                </Link>
+
+            </div>
+            <div className="container">
+
+                <Link to=".">
+                    <img src="/assets/images/cate_imgs/phone.png" alt="" />
+                    <span>Mobiles</span>
+                </Link>
+            </div>
+            <div className="container">
+
+                <Link to=".">
+                    <img src="/assets/images/cate_imgs/electronics.png" alt="" />
+                    <span>Electronics</span>
+                </Link>
+            </div>
+            <div className="container">
+                <Link to=".">
+                    <img src="/assets/images/cate_imgs/sports.png" alt="" />
+                    <span>Sports</span>
+                </Link>
+
+            </div>
+            <div className="container">
+
+                <Link to=".">
+                    <img src="/assets/images/cate_imgs/toys.png" alt="" />
+                    <span>Toys</span>
+                </Link>
+            </div>
+            <div className="container">
+
+                <Link to=".">
+                    <img src="/assets/images/cate_imgs/grocery.png" alt="" />
+                    <span>Grocery</span>
+                </Link>
+            </div>
+            <div className="container">
+
+                <Link to=".">
+                    <img src="/assets/images/cate_imgs/beauty.png" alt="" />
+                    <span>Beauty</span>
+                </Link>
+            </div>
         </div>
     );
 }
+

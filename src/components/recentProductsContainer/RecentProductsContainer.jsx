7df2979a5aef657c0ff49f786f7b7bc2b8563077
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2024-01-18 18:51:51 +0530

    few ui changes

--- src/components/recentProductsContainer/RecentProductsContainer.jsx
+++ src/components/recentProductsContainer/RecentProductsContainer.jsx
@@ -3,10 +3,20 @@ import "./recentProductsContainer.css"
 export default function RecentProductsContainer() {
     return (
         <div className="recentProductsContainer">
-            <div className="innerContainer">
-                <div className="heading">
+            <div className="heading">
                     <h3>Recent Products:</h3>
                 </div>
+            <div className="innerContainer">
+                
+                <div className="content">
+                    <div className="indiContent">content 1</div>
+                    <div className="indiContent">content 2</div>
+                    <div className="indiContent">content 3</div>
+                    <div className="indiContent">content 4</div>
+                </div>
+            </div>
+            {/* <div className="innerContainer">
+                
                 <div className="content">
                     <div className="indiContent">content 1</div>
                     <div className="indiContent">content 2</div>
@@ -15,9 +25,7 @@ export default function RecentProductsContainer() {
                 </div>
             </div>
             <div className="innerContainer">
-                <div className="heading">
-                    <h3>Recent Products:</h3>
-                </div>
+                
                 <div className="content">
                     <div className="indiContent">content 1</div>
                     <div className="indiContent">content 2</div>
@@ -26,27 +34,14 @@ export default function RecentProductsContainer() {
                 </div>
             </div>
             <div className="innerContainer">
-                <div className="heading">
-                    <h3>Recent Products:</h3>
-                </div>
+                
                 <div className="content">
                     <div className="indiContent">content 1</div>
                     <div className="indiContent">content 2</div>
                     <div className="indiContent">content 3</div>
                     <div className="indiContent">content 4</div>
                 </div>
-            </div>
-            <div className="innerContainer">
-                <div className="heading">
-                    <h3>Recent Products:</h3>
-                </div>
-                <div className="content">
-                    <div className="indiContent">content 1</div>
-                    <div className="indiContent">content 2</div>
-                    <div className="indiContent">content 3</div>
-                    <div className="indiContent">content 4</div>
-                </div>
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -16,8 +16,7 @@ export default function Home() {
                 <HomeBanner />
                 <RecentProductsContainer />
                 <HomeSuggetsProducts />
-                <HomeSuggetsProducts />
-                <HomeSuggetsProducts />
+                
             </div> 
 
         </div>
